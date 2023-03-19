4864e0fc0a0ea7e4f4a211007e8a35dc24bb8a6c
Adinortey Lawerteh <adinortey360@gmail.com>
2023-03-19 23:56:50 +0000

    remove stopwords from user input

--- generate.py
+++ generate.py
@@ -1,31 +1,51 @@
+import os
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-# Define a dictionary of articles
-articles = {
-    "article1": "Mental Health and Its Importance\nMental health refers to the overall psychological well-being of a person. It involves the way a person thinks, feels, and behaves. Mental health is important for everyone, as it affects our ability to cope with stress, make meaningful connections with others, and lead fulfilling lives. If you are experiencing mental health issues, it is important to seek help and support from a healthcare professional.",
-    "article2": "Common Mental Health Disorders\nThere are several common mental health disorders, including depression, anxiety, and bipolar disorder. These disorders can have a significant impact on a person's quality of life, making it difficult to perform everyday tasks and maintain healthy relationships. Treatment options for these disorders include medication, therapy, and lifestyle changes.",
-    "article3": "Self-Care for Mental Health\nSelf-care is an important aspect of maintaining good mental health. It involves taking care of your physical, emotional, and spiritual needs. Some self-care practices include getting enough sleep, eating a healthy diet, exercising regularly, and engaging in activities that bring you joy and fulfillment. Practicing self-care can help reduce stress, improve mood, and promote overall well-being.",
-    "article4": "Stigma and Mental Health\nStigma is a major barrier to seeking help for mental health issues. It can cause people to feel ashamed or embarrassed about their symptoms, and may prevent them from seeking the care and support they need. It is important to recognize that mental health issues are common and treatable, and to promote understanding and acceptance of those who are struggling with mental health challenges."
-}
+# Define a function to read text files from a directory and concatenate them
+def read_corpus(folder_path):
+    corpus_text = ""
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            with open(os.path.join(folder_path, filename), "r") as f:
+                corpus_text += f.read()
+    return corpus_text
 
-# Define a function to match user input to an article
-def match_article(input_text):
-    # Remove punctuation and convert to lowercase
+# Read the corpus from the "corpus" folder
+corpus_folder = "corpus"
+corpus_text = read_corpus(corpus_folder)
+
+# Split the corpus text into paragraphs
+paragraphs = re.split(r'\n\s*\n', corpus_text)
+
+# Define a function to match user input to a paragraph
+def match_paragraph(input_text):
+    # Remove punctuation, convert to lowercase, and tokenize the input text
     input_text = re.sub(r'[^\w\s]', '', input_text).lower()
+    input_tokens = word_tokenize(input_text)
+    # Remove stop words from the input tokens
+    stop_words = set(stopwords.words('english'))
+    input_tokens = [word for word in input_tokens if not word in stop_words]
     # Initialize variables to keep track of the best match so far
     best_match_score = 0
-    best_match_article = "I'm sorry, I couldn't find an article that matches your query."
-    # Loop through each article and calculate a score based on word matches
-    for article_key, article_text in articles.items():
-        article_score = sum([1 for word in article_text.split() if word.lower() in input_text])
-        if article_score > best_match_score:
-            best_match_score = article_score
-            best_match_article = article_text
-    # Return the best matching article
-    return best_match_article
+    best_match_paragraph = "I'm sorry, I couldn't find a paragraph that matches your query."
+    # Loop through each paragraph and calculate a score based on word matches
+    for paragraph in paragraphs:
+        # Remove stop words from the paragraph
+        paragraph_tokens = word_tokenize(paragraph)
+        paragraph_tokens = [word for word in paragraph_tokens if not word in stop_words]
+        # Calculate a score based on word matches
+        paragraph_score = sum([1 for word in paragraph_tokens if word.lower() in input_tokens])
+        if paragraph_score > best_match_score:
+            best_match_score = paragraph_score
+            best_match_paragraph = paragraph
+    # Return the best matching paragraph
+    return best_match_paragraph
 
-# Test the search engine
+# Start the chatbot
+print("Hello! I'm a mental health chatbot. Please enter your query below.")
 while True:
-    user_input = input("Please enter a query about mental health: ")
-    best_match_article = match_article(user_input)
-    print("The best article that matches your query is:\n", best_match_article)
+    user_input = input("you: ")
+    best_match_paragraph = match_paragraph(user_input)
+    print("bot:", best_match_paragraph)
