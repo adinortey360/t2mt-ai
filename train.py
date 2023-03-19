13bf4d38258507d69a0c3d3ec98c3dc4ff9800b1
Adinortey Lawerteh <adinortey360@gmail.com>
2023-03-19 21:19:26 +0000

    remove punctuation, make lowercase

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import os
+import json
+import string
 
 def extract_sentences_from_file(filename):
     with open(filename, 'r') as f:
@@ -16,6 +18,27 @@ def extract_sentences_from_corpus(corpus_folder):
                 sentences.extend(file_sentences)
     return sentences
 
+def find_unique_words(sentences):
+    unique_words = {}
+    translator = str.maketrans('', '', string.punctuation)
+    for sentence in sentences:
+        sentence = sentence.translate(translator)
+        words = sentence.lower().split()
+        for i, word in enumerate(words):
+            if word not in unique_words:
+                unique_words[word] = {
+                    "count": 1,
+                    "preceding_words": []
+                }
+            else:
+                unique_words[word]["count"] += 1
+            if i > 0:
+                unique_words[word]["preceding_words"].append(words[:i])
+    return unique_words
+
 corpus_folder = "corpus"
 sentences = extract_sentences_from_corpus(corpus_folder)
-print(sentences)
+unique_words = find_unique_words(sentences)
+
+with open("models/unique_words.json", "w") as outfile:
+    json.dump(unique_words, outfile, indent=4)
